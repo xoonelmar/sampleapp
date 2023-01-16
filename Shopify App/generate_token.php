72629ac090ca821460986c32b4232e76d3fd9745
xoonelmar <122776054+xoonelmar@users.noreply.github.com>
2023-01-16 15:33:15 +0800

    Add files via upload

---
+++ Shopify App/generate_token.php
@@ -0,0 +1,49 @@
+<?php
+
+// Get our helper functions
+require_once("inc/functions.php");
+
+// Set variables for our request
+$api_key = "717d72522206c2797aecd07e28b91e0f";
+$shared_secret = "e36b77d6e81abc008315dae532669d47";
+$params = $_GET; // Retrieve all request parameters
+$hmac = $_GET['hmac']; // Retrieve HMAC request parameter
+
+$params = array_diff_key($params, array('hmac' => '')); // Remove hmac from params
+ksort($params); // Sort params lexographically
+
+$computed_hmac = hash_hmac('sha256', http_build_query($params), $shared_secret);
+
+// Use hmac data to check that the response is from Shopify or not
+if (hash_equals($hmac, $computed_hmac)) {
+
+	// Set variables for our request
+	$query = array(
+		"client_id" => $api_key, // Your API key
+		"client_secret" => $shared_secret, // Your app credentials (secret key)
+		"code" => $params['code'] // Grab the access key from the URL
+	);
+
+	// Generate access token URL
+	$access_token_url = "https://" . $params['shop'] . "/admin/oauth/access_token";
+
+	// Configure curl client and execute request
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_URL, $access_token_url);
+	curl_setopt($ch, CURLOPT_POST, count($query));
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($query));
+	$result = curl_exec($ch);
+	curl_close($ch);
+
+	// Store the access token
+	$result = json_decode($result, true);
+	$access_token = $result['access_token'];
+
+	// Show the access token (don't do this in production!)
+	// echo $access_token; // Nelmar just put it a comment because we gonna be publishing the app
+
+} else {
+	// Someone is trying to be shady!
+	die('This request is NOT from Shopify!');
+}
---
+++ Shopify App/install.php
@@ -0,0 +1,14 @@
+<?php
+
+// Set variables for our request
+$shop = $_GET['shop'];
+$api_key = "717d72522206c2797aecd07e28b91e0f";
+$scopes = "read_orders,write_products";
+$redirect_uri = "http://localhost/generate_token.php";
+
+// Build install/approval URL to redirect to
+$install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
+
+// Redirect
+header("Location: " . $install_url);
+die();
